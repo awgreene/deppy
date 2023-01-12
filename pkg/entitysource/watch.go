3c294f3e9fd9bc8b9730cf2320b76940885c759a
Alexander Greene <greene.al1991@gmail.com>
2023-01-12 09:48:49 -0800

    wip list bundles

--- pkg/entitysource/watch.go
+++ pkg/entitysource/watch.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/go-logr/logr"
 	"github.com/operator-framework/api/pkg/operators/v1alpha1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/watch"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -32,22 +31,53 @@ func (r *DeppyRegistryV1Cache) StartCache() {
 		select {
 		case entry := <-r.watch.ResultChan():
 			r.logger.V(1).Info("New catalogSource event", "catalogsource", entry.Object, "event", entry.Type)
+			cs, ok := entry.Object.(*v1alpha1.CatalogSource)
+			if !ok {
+				r.logger.V(1).Info(typecastErrorMessage)
+				return
+			}
+
 			switch entry.Type {
 			case watch.Deleted:
 				r.Mutex.Lock()
 				defer r.Mutex.Unlock()
-				delete(r.cache, namespacedName(entry.Object))
-			case watch.Added:
-			case watch.Modified:
+				delete(r.cache, namespacedName(cs))
+			case watch.Added, watch.Modified:
+				r.Mutex.Lock()
+				defer r.Mutex.Unlock()
+				needsUpdate, err := r.cacheNeedsUpdate(cs)
+				if err != nil {
+					r.logger.V(1).Info(typecastErrorMessage)
+					return
+				}
+
+				if !needsUpdate {
+					r.logger.V(1).Info("Cache is up to date", "CatalogSource", namespacedName(cs))
+					return
+				}
+
+				// Update cache
+
 			}
 		}
 	}
 }
 
-func namespacedName(o runtime.Object) string {
-	catsrc, ok := o.(*v1alpha1.CatalogSource)
-	if !ok {
-		return ""
-	}
-	return types.NamespacedName{Namespace: catsrc.GetNamespace(), Name: catsrc.GetName()}.String()
+func namespacedName(cs *v1alpha1.CatalogSource) string {
+	return types.NamespacedName{Namespace: cs.GetNamespace(), Name: cs.GetName()}.String()
+}
+
+const (
+	typecastErrorMessage = "unable to convert event object into a catalogSource"
+)
+
+func (r *DeppyRegistryV1Cache) cacheNeedsUpdate(cs *v1alpha1.CatalogSource) (bool, error) {
+	return true, nil
+}
+
+func (r *DeppyRegistryV1Cache) retrieveBundles(cs *v1alpha1.CatalogSource) ([]Entity, error) {
+	svc := cs.Spec.Address
+
+	// use svc to retrieve entities
+	return nil, nil
 }
